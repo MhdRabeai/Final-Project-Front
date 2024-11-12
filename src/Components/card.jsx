c6aba8c962854db28749ad037ea51f262fbbdc61
MhdRabeai <mhd.rabea.naser@gmail.com>
2024-11-13 01:14:36 +0300

    updates

--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLock, faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
 
 const Card = ({ headline, content }) => {
   return (
