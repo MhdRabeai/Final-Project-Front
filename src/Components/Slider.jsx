c03101a587491b9c027160f2be0a6cf591847661
MhdRabeai <mhd.rabea.naser@gmail.com>
2024-11-13 02:50:05 +0300

    updates

---
+++ src/Components/Slider.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import {
+  Navigation,
+  Pagination,
+  Mousewheel,
+  Keyboard,
+  Virtual,
+} from "swiper/modules";
+
+const Slider = () => {
+  //   const [swiperRef, setSwiperRef] = useState(null);
+  //   const slideTo = (index) => {
+  //     swiperRef.slideTo(index - 1, 0);
+  //   };
+  return (
+    <>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        navigation={true}
+        onSwiper={(e) => console.log(e)}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Virtual, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+      {/* <p className="append-buttons">
+        <button onClick={() => prepend()} className="prepend-2-slides">
+          Prepend 2 Slides
+        </button>
+        <button onClick={() => slideTo(1)} className="prepend-slide">
+          Slide 1
+        </button>
+        <button onClick={() => slideTo(250)} className="slide-250">
+          Slide 250
+        </button>
+        <button onClick={() => slideTo(500)} className="slide-500">
+          Slide 500
+        </button>
+        <button onClick={() => append()} className="append-slides">
+          Append Slide
+        </button>
+      </p> */}
+    </>
+  );
+};
+
+export default Slider;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,42 +3,47 @@
 import React from "react";
 import cardsData from "../Components/cardsData";
 import Card from "../Components/card";
+import Slider from "../Components/Slider";
 
 export const Home = () => {
   return (
-    <div class="max-w-[86rem] mx-auto px-4">
-      <div class="grid lg:grid-cols-7 lg:gap-x-4 xl:gap-x-12  lg:items-center pb-10">
-        <div class="lg:col-span-3 flex flex-col ">
-          <h1 class="block text-3xl font-semibold text-gray-800 sm:text-4xl md:text-5xl lg:text-6xl dark:text-white text-center lg:text-left">
+    <div className="max-w-[86rem] mx-auto px-4">
+      <div className="grid lg:grid-cols-7 lg:gap-x-4 xl:gap-x-12  lg:items-center pb-10">
+        <div className="lg:col-span-3 flex flex-col ">
+          <h1 className="block text-3xl font-semibold text-gray-800 sm:text-4xl md:text-5xl lg:text-6xl dark:text-white text-center lg:text-left">
             Your Journey to Mental Clarity
           </h1>
-          <h1 class="block text-3xl font-semibold text-[#4f9451] sm:text-4xl md:text-5xl lg:text-6xl  text-center lg:text-left">
+          <h1 className="block text-3xl font-semibold text-[#4f9451] sm:text-4xl md:text-5xl lg:text-6xl  text-center lg:text-left">
             Starts Here
           </h1>
 
-          <ul class="mt-5 marker:text-[#4f9451] list-disc ps-5 space-y-1 text-sm text-gray-600 dark:text-neutral-400">
+          <ul className="mt-5 marker:text-[#4f9451] list-disc ps-5 space-y-1 text-sm text-gray-600 dark:text-neutral-400">
             <li>Take control of your health</li>
             <li>Seek expert guidance</li>
             <li>Access preventive care</li>
             <li>Find solutions for long-standing health concerns</li>
             <li>Recieve Support with Doctor X</li>
           </ul>
-          <div class="mt-5 lg:mt-8 flex ">
+          <div className="mt-5 lg:mt-8 flex ">
             <button
               type="button"
-              class="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-[#4f9451] text-white hover:bg-[rgba(79,148,81,0.84)] focus:outline-none focus:bg-[rgba(79,148,81,0.84)] disabled:opacity-50 disabled:pointer-events-none"
+              className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-[#4f9451] text-white hover:bg-[rgba(79,148,81,0.84)] focus:outline-none focus:bg-[rgba(79,148,81,0.84)] disabled:opacity-50 disabled:pointer-events-none"
             >
               Start therapy
             </button>
           </div>
         </div>
 
-        <div class="lg:col-span-4 mt-10 lg:mt-0">
-          <img class="w-full rounded-3xl " src="hero.png" alt="Hero Image" />
+        <div className="lg:col-span-4 mt-10 lg:mt-0">
+          <img
+            className="w-full rounded-3xl "
+            src="hero.png"
+            alt="Hero Image"
+          />
         </div>
       </div>
-      <div class="grid lg:grid-cols-7 lg:gap-x-4 xl:gap-x-12  lg:items-center pb-10 justify-between">
-        <div class="lg:col-span-2 flex  ">
+      <div className="grid lg:grid-cols-7 lg:gap-x-4 xl:gap-x-12  lg:items-center pb-10 justify-between">
+        <div className="lg:col-span-2 flex  ">
           <div>
             <div className="w-full">
               <span className="block mb-1 text-lg font-semibold text-[#4F9451]">
@@ -54,10 +59,10 @@ export const Home = () => {
                 your immediate concerns but also empowering you to thrive in
                 every aspect of your life.
               </p>
-              <div class="mt-5 lg:mt-8 flex ">
+              <div className="mt-5 lg:mt-8 flex ">
                 <button
                   type="button"
-                  class=" py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-[#4f9451] text-white hover:bg-[rgba(79,148,81,0.84)] focus:outline-none focus:bg-[rgba(79,148,81,0.84)] disabled:opacity-50 disabled:pointer-events-none"
+                  className=" py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-[#4f9451] text-white hover:bg-[rgba(79,148,81,0.84)] focus:outline-none focus:bg-[rgba(79,148,81,0.84)] disabled:opacity-50 disabled:pointer-events-none"
                 >
                   Read More
                 </button>
@@ -81,6 +86,82 @@ export const Home = () => {
           </div>
         </div>
       </div>
+      <Slider />
+      {/* <div
+        data-hs-carousel='{
+        "loadingclassNamees": "opacity-0"
+      }'
+        className="relative"
+      >
+        <div className="hs-carousel relative overflow-hidden w-full min-h-[350px] bg-white rounded-lg">
+          <div className="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
+            <div className="hs-carousel-slide">
+              <div className="flex justify-center h-full bg-gray-100 p-6">
+                <span className="self-center text-4xl transition duration-700">
+                  First slide
+                </span>
+              </div>
+            </div>
+            <div className="hs-carousel-slide">
+              <div className="flex justify-center h-full bg-gray-200 p-6">
+                <span className="self-center text-4xl transition duration-700">
+                  Second slide
+                </span>
+              </div>
+            </div>
+            <div className="hs-carousel-slide">
+              <div className="flex justify-center h-full bg-gray-300 p-6">
+                <span className="self-center text-4xl transition duration-700">
+                  Third slide
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <button
+          type="button"
+          className="hs-carousel-prev hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/[.1]"
+        >
+          <span className="text-2xl" aria-hidden="true">
+            <svg
+              className="w-4 h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+              />
+            </svg>
+          </span>
+          <span className="sr-only">Previous</span>
+        </button>
+        <button
+          type="button"
+          className="hs-carousel-next hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/[.1]"
+        >
+          <span className="sr-only">Next</span>
+          <span className="text-2xl" aria-hidden="true">
+            <svg
+              className="w-4 h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+              />
+            </svg>
+          </span>
+        </button>
+      </div> */}
     </div>
   );
 };
