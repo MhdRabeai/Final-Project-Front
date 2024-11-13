755da03f84699ccf261dbf3ffa125ba3bbf50625
NadaAlahdab <nadaalahdab4@gmail.com>
2024-11-13 16:39:30 +0300

    blogs now

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -1,8 +1,7 @@
-// src/pages/Blog.js
 import React, { useState, useEffect } from "react";
 import BlogCard from "../Components/BlogCard";
 
-const Blog = () => {
+const Blogs = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
@@ -11,9 +10,7 @@ const Blog = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/posts"
-        );
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
         const result = await response.json();
         setData(result);
       } catch (error) {
@@ -39,7 +36,7 @@ const Blog = () => {
 
   return (
     <section className="dark:bg-black">
-      <div className="container">
+      <div className="container mx-auto px-4">
         <div className="flex flex-wrap pb-10">
           <div className="w-full px-4">
             <div className="mx-auto max-w-[510px] text-center">
@@ -57,10 +54,12 @@ const Blog = () => {
           </div>
         </div>
 
-        <div className="mx-4 flex flex-wrap justify-center dark:bg-black dark:border-white">
-          {currentTeamMembers.map((post, index) => (
+        {/* عرض الكروت باستخدام grid */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {currentTeamMembers.map((post) => (
             <BlogCard
-              key={index}
+              key={post.id}
+              id={post.id}
               date={new Date().toLocaleDateString()}
               CardTitle={post.title}
               CardDescription={post.body.substring(0, 100) + "..."}
@@ -69,6 +68,7 @@ const Blog = () => {
           ))}
         </div>
 
+        {/* التصفح بين الصفحات */}
         {data.length >= itemsPerPage && (
           <div className="pt-10 text-center dark:bg-black">
             <div className="inline-flex justify-center rounded bg-white p-3 shadow-[0px_1px_3px_0px_rgba(0,0,0,0.13)] dark:bg-black dark:border">
@@ -127,7 +127,7 @@ const Blog = () => {
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
-                        d="M18 10L11.5312 3.4375C11.25 3.15625 10.8125 3.15625 10.5312 3.4375C10.25 3.71875 10.25 4.15625 10.5312 4.4375L15.7812 9.78125H2.5C2.125 9.78125 1.8125 10.0937 1.8125 10.4688C1.8125 10.8438 2.125 11.1875 2.5 11.1875H15.8437L10.5312 16.5938C10.25 16.875 10.25 17.3125 10.5312 17.5938C10.6562 17.7188 10.8437 17.7812 11.0312 17.7812C11.2187 17.7812 11.4062 17.7188 11.5312 17.5625L18 11C18.2812 10.7187 18.2812 10.2812 18 10Z"
+                        d="M17.5 9.8125H4.15625L9.46875 4.40625C9.75 4.125 9.75 3.6875 9.46875 3.40625C9.1875 3.125 8.75 3.125 8.46875 3.40625L2 9.96875C1.71875 10.25 1.71875 10.6875 2 10.9688L8.46875 17.5312C8.59375 17.6562 8.78125 17.75 8.96875 17.75C9.15625 17.75 9.3125 17.6875 9.46875 17.5625C9.75 17.2812 9.75 16.8438 9.46875 16.5625L4.1875 11.2188H17.5C17.875 11.2188 18.1875 10.9062 18.1875 10.5312C18.1875 10.125 17.875 9.8125 17.5 9.8125Z"
                         fill="currentColor"
                       />
                     </svg>
@@ -142,4 +142,4 @@ const Blog = () => {
   );
 };
 
-export default Blog;
+export default Blogs;
