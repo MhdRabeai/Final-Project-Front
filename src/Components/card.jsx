5eb3d03c1a1464b9cbb626a4b8345df75134ebdf
Yara Mamary <yaramamary10@gmail.com>
2024-11-16 14:53:44 +0300

    responsiveCard2

--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Card = ({ headline, content, icon }) => {
   return (
-    <div className="bg-white border border-gray-200 rounded-lg p-6 shadow-md hover:shadow-lg transition-transform transform hover:scale-105 w-full flex flex-col justify-between homeFeature">
+    <div className="bg-white border border-gray-200 rounded-lg p-6 shadow-md hover:shadow-lg transition-transform transform hover:scale-105 w-full min-h-[250px] sm:min-h-[300px] md:min-h-[350px] lg:min-h-[250px] lg:h-[300px] flex flex-col justify-between homeFeature overflow-hidden">
       <div className="text-3xl text-[#4F9451]">
         <FontAwesomeIcon icon={icon} />
       </div>
