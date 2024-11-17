5299c0b08df7b6bb7a6984bf1ffdd9814e6b5b62
MhdRabeai <mhd.rabea.naser@gmail.com>
2024-11-18 02:08:38 +0300

    ...

--- src/Components/MultiStepForm.jsx
+++ src/Components/MultiStepForm.jsx
@@ -60,18 +60,21 @@ const MultiStepForm = () => {
           Book a Doctor's Appointment
         </h1>
 
-        <div className="mb-6 flex items-center justify-between">
+        <ol className="flex md:w-full items-center text-indigo-600  sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-4 xl:after:mx-8 ">
           {["Step 1", "Step 2", "Step 3", "Step 4"].map((label, index) => (
-            <div
+            <li
               key={index}
-              className={`w-8 h-8 flex items-center justify-center rounded-full text-white ${
+              className={` flex items-center whitespace-nowrap after:content-['/'] sm:after:hidden after:mx-2  ${
                 currentStep === index + 1 ? "bg-blue-600" : "bg-gray-400"
               }`}
             >
-              {index + 1}
-            </div>
+              <span class="w-6 h-6 bg-indigo-600 border border-indigo-200 rounded-full flex justify-center items-center mr-3 text-sm text-white lg:w-10 lg:h-10">
+                {index + 1}
+              </span>
+              Step {index + 1}
+            </li>
           ))}
-        </div>
+        </ol>
         {renderStep()}
       </div>
     </div>
