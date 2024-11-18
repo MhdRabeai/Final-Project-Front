57e42c62b5fd99c17490f2113237a3125d819a3c
NadaAlahdab <nadaalahdab4@gmail.com>
2024-11-18 07:33:16 +0300

    ...

--- src/Pages/TabelPataints.jsx
+++ src/Pages/TabelPataints.jsx
@@ -91,20 +91,20 @@ const TabelPataints = () => {
     navigate("/dashboard/admin/CreateAccountDoctor");
   };
   return (
-    <div className="flex justify-center px-5 h-full">
-      <div className="overflow-x-auto content-center w-full max-w-7xl">
+    <div className="flex flex-col justify-center items-center px-4 sm:px-6 lg:px-8 h-full">
+      <div className="overflow-x-auto w-full max-w-7xl">
         <div className="min-w-full inline-block align-middle border border-gray-300 rounded-lg">
           <div className="py-5 px-4 relative">
-            <h1 className="text-2xl font-bold text-center">All Doctors</h1>
+            <h1 className="text-xl sm:text-2xl font-bold text-center my-4">All Pataint</h1>
             <button
               onClick={handleAddPataint}
-              className="px-4 py-2 text-white bg-[#4F9451] rounded-lg hover:bg-[#4F9451] transition duration-300 absolute top-4 right-4"
+              className="px-4 py-2 w-full sm:w-auto text-center text-white bg-[#4F9451] rounded-lg hover:bg-[#3a763e] transition duration-300"
             >
               Add Pataint
             </button>
           </div>
-          <div className="relative max-w-xs">
-            <label htmlFor="search-input" className="sr-only">
+          <div className="relative max-w-xs sm:max-w-md lg:max-w-lg mx-auto my-4">
+            <label htmlFor="p-5 search-input" className="sr-only">
               Search
             </label>
             <input
@@ -116,7 +116,7 @@ const TabelPataints = () => {
               placeholder="Search for items"
             />
           </div>
-          <div className="overflow-hidden ">
+          <div className="overflow-x-auto">
             {sortedData.length === 0 ? (
               <div className="flex justify-center items-center h-full">
                 <img
@@ -126,80 +126,42 @@ const TabelPataints = () => {
                 />
               </div>
             ) : (
-              <table className="min-w-full table-auto">
-                <thead className="border-y border-gray-200 dark:border-neutral-700">
+              <table className="min-w-full table-auto text-sm sm:text-base">
+                <thead className="border-y border-gray-200 dark:border-neutral-700 bg-[#4F9451]">
                   <tr>
                     <th></th>
-                    <th className="py-3 px-2 text-left font-medium text-[#4F9451]">
-                      <button
-                        onClick={() => handleSort("name")}
-                        className="text-left"
-                      >
-                        Name
-                      </button>
+                    <th className="py-3 px-2 text-left font-medium text-white">
+                      <button onClick={() => handleSort("name")} className="text-left text-white">Name</button>
                     </th>
-                    <th className="py-3 px-2 text-left font-medium text-[#4F9451]">
-                      <button
-                        onClick={() => handleSort("age")}
-                        className="text-left"
-                      >
-                        Age
-                      </button>
+                    <th className="py-3 px-2 text-left font-medium text-white">
+                      <button onClick={() => handleSort("age")} className="text-left text-white">Age</button>
                     </th>
-                    <th className="py-3 px-2 text-left font-medium text-[#4F9451]">
-                      <button
-                        onClick={() => handleSort("address")}
-                        className="text-left"
-                      >
-                        Address
-                      </button>
+                    <th className="py-3 px-2 text-left font-medium text-white hidden lg:table-cell">
+                      <button onClick={() => handleSort("address")} className="text-left text-white">Address</button>
                     </th>
-                    <th className="py-3 px-2 text-left font-medium text-[#4F9451]">
-                      <button
-                        onClick={() => handleSort("email")}
-                        className="text-left"
-                      >
-                        Email
-                      </button>
+                    <th className="py-3 px-2 text-left font-medium text-white">
+                      <button onClick={() => handleSort("email")} className="text-left text-white">Email</button>
                     </th>
-                    <th className="py-3 px-2 text-left font-medium text-[#4F9451]">
-                      <button
-                        onClick={() => handleSort("phonenumber")}
-                        className="text-left"
-                      >
-                        Phone Number
-                      </button>
+                    <th className="py-3 px-2 text-left font-medium text-white hidden sm:table-cell">
+                      <button onClick={() => handleSort("phonenumber")} className="text-left text-white">Phone Number</button>
                     </th>
-                    <th className="py-3 px-2 text-left font-medium text-[#4F9451]">
-                      <button
-                        onClick={() => handleSort("gender")}
-                        className="text-left"
-                      >
-                        Gender
-                      </button>
+                    <th className="py-3 px-2 text-left font-medium text-white hidden sm:table-cell">
+                      <button onClick={() => handleSort("gender")} className="text-left text-white">Gender</button>
                     </th>
-                    <th className="py-3 px-5 text-right font-medium text-[#4F9451]">
-                      Action
-                    </th>
+                    <th className="py-3 px-2 text-left font-medium text-white hidden md:table-cell">Action</th>
                   </tr>
                 </thead>
+
                 <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
                   {sortedData.map((row) => (
-                    <tr
-                      key={row.name}
-                      className="border-b border-gray-300 dark:border-neutral-700"
-                    >
+                    <tr key={row.name} className="border-b border-gray-300 dark:border-neutral-700">
                       <td className="py-3 px-3">
-                        <div className="flex items-center h-5">
-                          <input
-                            type="checkbox"
-                            name="challenges"
-                            value={"Family"}
-                            className="shrink-0 mt-0.5 border-gray-200 rounded text-[#4f9451] focus:ring-[#4f9451] disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-[#4f9451] dark:checked:border-[#4f9451] dark:focus:ring-offset-gray-800"
-                            id="checkbox-group-12"
-                            defaultChecked
-                          />
-                        </div>
+                        <input
+                          type="checkbox"
+                          name="challenges"
+                          value={"Family"}
+                          className="shrink-0 mt-0.5 border-gray-200 rounded text-[#4f9451] focus:ring-[#4f9451] dark:bg-neutral-800 dark:border-neutral-700"
+                        />
                       </td>
                       <td className="p-3 text-sm text-gray-800 dark:text-neutral-200">
                         {editRow === row.name ? (
@@ -227,7 +189,7 @@ const TabelPataints = () => {
                           row.age
                         )}
                       </td>
-                      <td className="p-3 text-sm text-gray-800 dark:text-neutral-200">
+                      <td className="p-3 text-sm text-gray-800 dark:text-neutral-200 hidden lg:table-cell">
                         {editRow === row.name ? (
                           <input
                             type="text"
@@ -253,7 +215,7 @@ const TabelPataints = () => {
                           row.email
                         )}
                       </td>
-                      <td className="p-3 text-sm text-gray-800 dark:text-neutral-200">
+                      <td className="p-3 text-sm text-gray-800 dark:text-neutral-200 hidden sm:table-cell">
                         {editRow === row.name ? (
                           <input
                             type="text"
@@ -266,7 +228,7 @@ const TabelPataints = () => {
                           row.phonenumber
                         )}
                       </td>
-                      <td className="p-3 text-sm text-gray-800 dark:text-neutral-200">
+                      <td className="p-3 text-sm text-gray-800 dark:text-neutral-200 hidden sm:table-cell">
                         {editRow === row.name ? (
                           <select
                             name="gender"
@@ -281,29 +243,24 @@ const TabelPataints = () => {
                           row.gender
                         )}
                       </td>
-                      <td className="pr-5 p-2 text-right">
+                      <td className="pr-5 p-2 text-right hidden md:table-cell">
                         {editRow === row.name ? (
-                          <span
-                            onClick={() => handleSave(row.name)}
-                            className="text-green-600 cursor-pointer hover:text-green-700"
-                          >
-                            <FontAwesomeIcon icon={faSave} />
-                          </span>
+                          <div className="bg-green-100 rounded-full p-2 cursor-pointer" onClick={() => handleSave(row.name)}>
+                            <FontAwesomeIcon icon={faSave} className="text-green-500" />
+                          </div>
                         ) : (
                           <>
-                            <span
-                              onClick={() => handleEdit(row)}
-                              className="text-blue-500 text-lg cursor-pointer hover:text-blue-700 mx-4"
-                            >
-                              <FontAwesomeIcon icon={faPencilAlt} />
-                            </span>
-                            <span
-                              onClick={() => handleDelete(row.name)}
-                              className="text-gray-500 text-lg cursor-pointer hover:text-gray-700"
-                            >
-                              <FontAwesomeIcon icon={faTrash} />
-                            </span>
+                            <div className="flex items-center">
+                              <div className="bg-blue-100 rounded-full p-2 cursor-pointer mx-2"
+                                onClick={() => handleEdit(row)}>
+                                <FontAwesomeIcon icon={faPencilAlt} className="text-blue-500" />
+                              </div>
+                              <div className="bg-red-100 rounded-full p-2 cursor-pointer" onClick={() => handleDelete(row.name)}>
+                                <FontAwesomeIcon icon={faTrash} className="text-red-500" />
+                              </div>
+                            </div>
                           </>
+
                         )}
                       </td>
                     </tr>
@@ -315,6 +272,7 @@ const TabelPataints = () => {
         </div>
       </div>
     </div>
+
   );
 };
 
